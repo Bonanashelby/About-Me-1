aa162679a85e1ffdfd2a1a90f9ae93b1141ad639
galang pham <phamgalang@gmail.com>
2017-02-08 17:55:58 -0800

    fourth

--- app.js
+++ app.js
@@ -16,43 +16,30 @@ var quizResponseOne = prompt('Please answer with Y or N for all the following qu
 
 if(quizResponseOne.toUpperCase() === 'Y'){
   console.log('Bingo! You are correct! Glen has a pomeranian named Molly, and she is the cutest dog in the world!');
-  alert ('Bingo! You are correct! Glen has a pomeranian named Molly, and she is the cutest dog in the world!');
 } else {
   console.log('Wrong! You are wrong. Glen does have a dog!');
-  alert('Wrong! You are wrong. Glen does have a dog!');
 }
-
 var quizResponseTwo = prompt('Does Glen like to fight?');
 if(quizResponseTwo.toUpperCase() === 'Y'){
   console.log('Correcto! But not innocent people, only for beating up static punching bags!');
-  alert('Correcto! But not innocent people, only for beating up static punching bags!');
 } else {
   console.log('Wrong! He likes to fight, but only towards punching bags!');
-  alert('Wrong! He likes to fight, but only towards punching bags!');
 }
-
 var quizResponseThree = prompt('Does Glen love electronic music??');
 if(quizResponseThree.toUpperCase() === 'Y'){
   console.log('Bingo! Glen LOVES electronic music and is an electronic music snob!');
-  alert('Bingo! Glen LOVES electronic music and is an electronic music snob!');
 } else {
   console.log('WRONG! Glen would be offended if you answered this wrong as he LOOOOOOVES electronic music!');
-  alert('WRONG! Glen would be offended if you answered this wrong as he LOOOOOOVES electronic music!');
 }
 var quizResponseFour = prompt('Does Glen have a girlfriend?');
 if(quizResponseFour.toUpperCase() === 'Y'){
   console.log('You are correct! But why are you asking this question nosy person?!?!');
-  alert('You are correct! But why are you asking this question nosy person?!?!');
 } else {
   console.log('Wrong! Are you trying to say he is ugly and has a horrible personality and is probably single!?');
-  alert('Wrong! Are you trying to say he is ugly and has a horrible personality and is probably single!?');
 }
-
 var quizResponseFive = prompt('Does Glen enjoy weightlifting?');
 if(quizResponseFive.toUpperCase() === 'Y'){
   console.log('Correct! He will take that as a compliment, because you are assuming he looks likes he lifts! Thank you!');
-  alert('Correct! He will take that as a compliment, because you are assuming he looks likes he lifts! Thank you!');
 } else {
   console.log('Wrong! Are you trying to say he looks small and pathetic and needs to workout!? He will be offended!');
-  alert('Wrong! Are you trying to say he looks small and pathetic and needs to workout!? He will be offended!');
 }
