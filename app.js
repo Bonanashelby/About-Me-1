2052292e8c33040509f680c0c1d573cc2231810f
galang pham <phamgalang@gmail.com>
2017-02-10 14:17:00 -0800

    sixth change

--- app.js
+++ app.js
@@ -16,38 +16,53 @@ var quizResponseOne = prompt('Please answer with Y or N for all the following qu
 
 if(quizResponseOne.toUpperCase() === 'Y'){
   alert('Bingo! You are correct! Glen has a pomeranian named Molly, and she is the cutest dog in the world!');
+  console.log('Bingo! You are correct! Glen has a pomeranian named Molly, and she is the cutest dog in the world!');
   score += 1;
 } else {
   alert('Wrong! You are wrong. Glen does have a dog!');
+  console.log('Wrong! You are wrong. Glen does have a dog!');
 }
+
 var quizResponseTwo = prompt('Does Glen like to fight?');
 if(quizResponseTwo.toUpperCase() === 'Y'){
   alert('Correcto! But not innocent people, only for beating up static punching bags!');
   score += 1;
+  console.log(Correcto! But not innocent people, only for beating up static punching bags!);
 } else {
   alert('Wrong! He likes to fight, but only towards punching bags!');
+  console.log('Wrong! He likes to fight, but only towards punching bags!');
 }
+
 var quizResponseThree = prompt('Does Glen love electronic music??');
 if(quizResponseThree.toUpperCase() === 'Y'){
   alert('Bingo! Glen LOVES electronic music and is an electronic music snob!');
   score += 1;
+  console.log('Bingo! Glen LOVES electronic music and is an electronic music snob!');
 } else {
   alert('WRONG! Glen would be offended if you answered this wrong as he LOOOOOOVES electronic music!');
+  console.log('WRONG! Glen would be offended if you answered this wrong as he LOOOOOOVES electronic music!');
 }
+
 var quizResponseFour = prompt('Does Glen have a girlfriend?');
 if(quizResponseFour.toUpperCase() === 'Y'){
-  alert('You are correct! But why are you asking this question nosy person?!?!');
+  alert('You are correct! But why do you want to know nosy person?!?!');
+  console.log('You are correct! But why do you want to know nosy person?!?!');
   score += 1;
 } else {
   alert('Wrong! Are you trying to say he is ugly and has a horrible personality and is probably single!?');
+  console.log('Wrong! Are you trying to say he is ugly and has a horrible personality and is probably single!?');
 }
+
 var quizResponseFive = prompt('Does Glen enjoy weightlifting?');
 if(quizResponseFive.toUpperCase() === 'Y'){
   alert('Correct! He will take that as a compliment, because you are assuming he looks likes he lifts! Thank you!');
+  console.log('Correct! He will take that as a compliment, because you are assuming he looks likes he lifts! Thank you!');
   score += 1;
 } else {
   alert('Wrong! Are you trying to say he looks small and pathetic and needs to workout!? He will be offended!');
+  console.log('Wrong! Are you trying to say he looks small and pathetic and needs to workout!? He will be offended!');
 }
+
 for (var i = 0; i < 4; i++) {
   var quizResponseSix = prompt('Finally, guess how old I am? You get four tries!');
   if(quizResponseSix == 30) {
@@ -60,6 +75,7 @@ for (var i = 0; i < 4; i++) {
     alert ('Too low! But thanks for the compliment!');
   }
 }
+
 var multiChoice = ['JAPAN', 'TAIWAN', 'INDONESIA', 'THAILAND'];
 for (var i = 0; i < 6; i++) {
   var isCorrect = false;
